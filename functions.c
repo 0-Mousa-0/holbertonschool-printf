ef2b4834291d8874d8ae48e009f6f7ff9eb0bbac
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-18 11:46:45 +0000

    Implement helper functions for _printf

--- functions.c
+++ functions.c
@@ -36,7 +36,121 @@ return (c);
 /**
  * print_number - prints an integer
  * @n: integer to print
+ *                                                                                                                                                                                                                                          #include "main.h"
+
+/**
+ * _putchar - prints a single character
+ * @c: the character to print
  *
+ * Return: number of bytes written (1), or -1 on error
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string
+ * @str: string to print
+ *
+ * Return: number of characters printed
+ */
+int print_string(char *str)
+{
+int c = 0;
+
+if (!str)
+str = "(null)";
+
+while (*str)
+{
+c += _putchar(*str);
+str++;
+}
+
+return (c);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ *#include "main.h"
+
+/**
+ * _putchar - prints a single character
+ * @c: the character to print
+ *
+ * Return: number of bytes written (1), or -1 on error
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string
+ * @str: string to print
+ *
+ * Return: number of characters printed
+ */
+int print_string(char *str)
+{
+int c = 0;
+
+if (!str)
+str = "(null)";
+
+while (*str)
+{
+c += _putchar(*str);
+str++;
+}
+
+return (c);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ *#include "main.h"
+
+/**
+ * _putchar - prints a single character
+ * @c: the character to print
+ *
+ * Return: number of bytes written (1), or -1 on error
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+/**
+ * print_string - prints a string
+ * @str: string to print
+ *
+ * Return: number of characters printed
+ */
+int print_string(char *str)
+{
+int c = 0;
+
+if (!str)
+str = "(null)";
+
+while (*str)
+{
+c += _putchar(*str);
+str++;
+}
+
+return (c);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ * 
  * Return: number of characters printed
  */
 int print_number(int n)
@@ -46,7 +160,7 @@ unsigned int no;
 
 if (n < 0)
 {
-c += _putchar('-');   /* FIXED: was '_' */
+c += putchar('-');   /* FIXED: was '' */
 no = -n;
 }
 else
@@ -58,3 +172,18 @@ c += _putchar(no % 10 + '0');
 
 return (c);
 }
+/**
+ * print_binary - prints an unsigned int in binary
+ * @n: number to print
+ * Return: number of digits printed
+ */
+int print_binary(unsigned int n)
+{
+int c = 0;
+if (n > 1)
+c += print_binary(n / 2);
+c += _putchar((n % 2) + '0');
+
+return (c);
+}
+
