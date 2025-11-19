60200faf45c8529807b62f9bdeb77914f1ee7ef6
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-19 03:46:50 +0000

    Update main.h header with function prototypes

--- main.h
+++ main.h
@@ -3,10 +3,11 @@
 
 #include <stdarg.h>
 #include <unistd.h>
+
 int _putchar(char c);
 int print_string(char *str);
 int print_number(int n);
+int print_binary(unsigned int n);
 int _printf(const char *format, ...);
-int print_binary(unsigned int n);
 
-#endif
+#endif /* MAIN_H */
