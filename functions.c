7470a369b65ccb2583b05f7096913c26cb8a0cd7
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-19 03:52:23 +0000

    update helper functions for _printf

--- functions.c
+++ functions.c
@@ -3,187 +3,70 @@
 /**
  * _putchar - prints a single character
  * @c: the character to print
- *
  * Return: number of bytes written (1), or -1 on error
  */
 int _putchar(char c)
 {
-return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 /**
  * print_string - prints a string
  * @str: string to print
- *
  * Return: number of characters printed
  */
 int print_string(char *str)
 {
-int c = 0;
+	int c = 0;
 
-if (!str)
-str = "(null)";
+	if (!str)
+		str = "(null)";
 
-while (*str)
-{
-c += _putchar(*str);
-str++;
-}
-
-return (c);
+	while (*str)
+	{
+		c += _putchar(*str);
+		str++;
+	}
+	return (c);
 }
 
 /**
  * print_number - prints an integer
  * @n: integer to print
- *                                                                                                                                                                                                                                          #include "main.h"
-
-/**
- * _putchar - prints a single character
- * @c: the character to print
- *
- * Return: number of bytes written (1), or -1 on error
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints a string
- * @str: string to print
- *
- * Return: number of characters printed
- */
-int print_string(char *str)
-{
-int c = 0;
-
-if (!str)
-str = "(null)";
-
-while (*str)
-{
-c += _putchar(*str);
-str++;
-}
-
-return (c);
-}
-
-/**
- * print_number - prints an integer
- * @n: integer to print
- *#include "main.h"
-
-/**
- * _putchar - prints a single character
- * @c: the character to print
- *
- * Return: number of bytes written (1), or -1 on error
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints a string
- * @str: string to print
- *
- * Return: number of characters printed
- */
-int print_string(char *str)
-{
-int c = 0;
-
-if (!str)
-str = "(null)";
-
-while (*str)
-{
-c += _putchar(*str);
-str++;
-}
-
-return (c);
-}
-
-/**
- * print_number - prints an integer
- * @n: integer to print
- *#include "main.h"
-
-/**
- * _putchar - prints a single character
- * @c: the character to print
- *
- * Return: number of bytes written (1), or -1 on error
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
-
-/**
- * print_string - prints a string
- * @str: string to print
- *
- * Return: number of characters printed
- */
-int print_string(char *str)
-{
-int c = 0;
-
-if (!str)
-str = "(null)";
-
-while (*str)
-{
-c += _putchar(*str);
-str++;
-}
-
-return (c);
-}
-
-/**
- * print_number - prints an integer
- * @n: integer to print
- * 
  * Return: number of characters printed
  */
 int print_number(int n)
 {
-int c = 0;
-unsigned int no;
+	int c = 0;
+	unsigned int num;
 
-if (n < 0)
-{
-c += putchar('-');   /* FIXED: was '' */
-no = -n;
+	if (n < 0)
+	{
+		c += _putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+
+	if (num / 10)
+		c += print_number(num / 10);
+	c += _putchar(num % 10 + '0');
+
+	return (c);
 }
-else
-no = n;
 
-if (no / 10)
-c += print_number(no / 10);
-c += _putchar(no % 10 + '0');
-
-return (c);
-}
 /**
  * print_binary - prints an unsigned int in binary
- * @n: number to print
+ * @n: number to print in binary
  * Return: number of digits printed
  */
 int print_binary(unsigned int n)
 {
-int c = 0;
-if (n > 1)
-c += print_binary(n / 2);
-c += _putchar((n % 2) + '0');
+	int c = 0;
 
-return (c);
+	if (n > 1)
+		c += print_binary(n / 2);
+
+	c += _putchar((n % 2) + '0');
+	return (c);
 }
-
