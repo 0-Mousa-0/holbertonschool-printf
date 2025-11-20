d6ce94c3a6815a24ee78932ac283aba81bf82291
0-Mousa-0 <mosa.alqarni01@gmail.com>
2025-11-20 14:56:00 +0000

    _printf functionality with 1024-char buffer

--- _printf.c
+++ _printf.c
@@ -21,9 +21,9 @@ int print_string(char *buf, int *idx, char *s)
     return count;
 }
 
-int print_int(char *buf, int *idx, long n)
+int print_int(char *buf, int *idx, int n)
 {
-    unsigned long v;
+    unsigned int num;
     int count = 0;
 
     if (n < 0)
@@ -31,36 +31,28 @@ int print_int(char *buf, int *idx, long n)
         if (buf_add(buf, idx, '-') == -1)
             return -1;
         count++;
-        v = -n;
+        num = -n;
     }
     else
-        v = n;
+        num = n;
 
-    if (v / 10)
-    {
-        int r = print_int(buf, idx, v / 10);
-        if (r == -1) return -1;
-        count += r;
-    }
+    if (num / 10)
+        count += print_int(buf, idx, num / 10);
 
-    if (buf_add(buf, idx, '0' + (v % 10)) == -1)
+    if (buf_add(buf, idx, (num % 10) + '0') == -1)
         return -1;
 
     return count + 1;
 }
 
-int print_base(char *buf, int *idx, unsigned int n, unsigned int base, const char *digits)
+/* New: print binary */
+int print_binary(char *buf, int *idx, unsigned int n)
 {
     int count = 0;
+    if (n > 1)
+        count += print_binary(buf, idx, n >> 1);
 
-    if (n / base)
-    {
-        int r = print_base(buf, idx, n / base, base, digits);
-        if (r == -1) return -1;
-        count += r;
-    }
-
-    if (buf_add(buf, idx, digits[n % base]) == -1)
+    if (buf_add(buf, idx, (n & 1) + '0') == -1)
         return -1;
 
     return count + 1;
@@ -71,6 +63,9 @@ int _printf(const char *format, ...)
     va_list ap;
     char buffer[BUF_SIZE];
     int idx = 0, count = 0;
+
+    if (!format)
+        return -1;
 
     va_start(ap, format);
 
@@ -96,17 +91,13 @@ int _printf(const char *format, ...)
                 count += print_int(buffer, &idx, va_arg(ap, int));
 
             else if (*format == 'b')
-                count += print_base(buffer, &idx, va_arg(ap, unsigned int), 2, "01");
+                count += print_binary(buffer, &idx, va_arg(ap, unsigned int));
 
             else if (*format == '%')
                 count += buf_add(buffer, &idx, '%');
 
             else
-            {
-                /* Unknown specifier → print % + specifier */
-                count += buf_add(buffer, &idx, '%');
                 count += buf_add(buffer, &idx, *format);
-            }
 
             format++;
         }
@@ -117,3 +108,4 @@ int _printf(const char *format, ...)
 
     return count;
 }
+
