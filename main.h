6d182540322f6adc699c642e751ef27483d4d5c5
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-20 10:25:40 +0000

    Update main.h with buffer support and all function prototypes

--- main.h
+++ main.h
@@ -1,24 +1,19 @@
+ 
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdint.h>
 
 #define BUF_SIZE 1024
 
 int _printf(const char *format, ...);
 
-/* Buffer functions */
 int buf_add(char *buf, int *idx, char c);
 int buf_flush(char *buf, int *idx);
 
-/* Print functions with buffer */
 int print_char(char *buf, int *idx, char c);
 int print_string(char *buf, int *idx, char *s);
-int print_number(char *buf, int *idx, int n);
-int print_binary(char *buf, int *idx, unsigned int n);
-int print_pointer(char *buf, int *idx, void *ptr);
-int print_custom_string(char *buf, int *idx, char *str);
+int print_int(char *buf, int *idx, int n);
 
 #endif
