fa771cfc527fca50b54e6e302253b9d04b0caf25
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-21 21:43:07 +0000

    ensure buffer functions are properly defined

--- buffun.c
+++ buffun.c
@@ -1,25 +1,31 @@
 #include "main.h"
 
 /**
- * buff_push - adds char to buffer; flushes when full
+ * buff_push - push character to buffer
+ * @c: character to push
+ * @buff: buffer
+ * @buff_i: buffer index pointer
+ * Return: 1 on success
  */
 int buff_push(char c, char *buff, int *buff_i)
 {
     if (*buff_i >= 1024)
         buff_flush(buff, buff_i);
-
+    
     buff[(*buff_i)++] = c;
-    return 1;
+    return (1);
 }
 
 /**
- * buff_flush - writes buffer content to stdout
+ * buff_flush - flush buffer to stdout
+ * @buff: buffer
+ * @buff_i: buffer index pointer
  */
-int buff_flush(char *buff, int *buff_i)
+void buff_flush(char *buff, int *buff_i)
 {
     if (*buff_i > 0)
+    {
         write(1, buff, *buff_i);
-
-    *buff_i = 0;
-    return 0;
+        *buff_i = 0;
+    }
 }
