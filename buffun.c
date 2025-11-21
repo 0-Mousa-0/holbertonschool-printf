01a0f42f354d60e5f4aeeaa1b688be5955a065c1
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-21 22:43:29 +0000

    implement buffer functions buff_push and buff_flush

--- buffun.c
+++ buffun.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
-/**
- * buff_push - push character to buffer
- * @c: character to push
- * @buff: buffer
- * @buff_i: buffer index pointer
- * Return: 1 on success
- */
-int buff_push(char c, char *buff, int *buff_i)
+#define BUFFER_SIZE 1024
+
+/* Forward declaration to avoid implicit declaration */
+void buff_flush(char *buff, int *buff_i);
+
+/* Add character to buffer */
+void buff_push(char *buff, int *buff_i, char c)
 {
-    if (*buff_i >= 1024)
+    if (*buff_i >= BUFFER_SIZE)
         buff_flush(buff, buff_i);
-    
+
     buff[(*buff_i)++] = c;
-    return (1);
 }
 
-/**
- * buff_flush - flush buffer to stdout
- * @buff: buffer
- * @buff_i: buffer index pointer
- */
+/* Flush buffer to stdout */
 void buff_flush(char *buff, int *buff_i)
 {
     if (*buff_i > 0)
@@ -29,3 +23,4 @@ void buff_flush(char *buff, int *buff_i)
         *buff_i = 0;
     }
 }
+
